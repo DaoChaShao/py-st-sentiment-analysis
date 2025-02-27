6bfa8833efd26bd7451080f077f30929c4306bef
DaoChaShao <27882272@qq.com>
2025-02-27 22:26:42 +1300

    feat: add main.py with initial implementation and sentiment analysis reference

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/27 22:18
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   main.py
+# @Desc     :
+"""
+@InProceedings{maas-EtAl:2011:ACL-HLT2011,
+  author    = {Maas, Andrew L.  and  Daly, Raymond E.  and  Pham, Peter T.  and  Huang, Dan  and  Ng, Andrew Y.  and  Potts, Christopher},
+  title     = {Learning Word Vectors for Sentiment Analysis},
+  booktitle = {Proceedings of the 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies},
+  month     = {June},
+  year      = {2011},
+  address   = {Portland, Oregon, USA},
+  publisher = {Association for Computational Linguistics},
+  pages     = {142--150},
+  url       = {http://www.aclweb.org/anthology/P11-1015}
+}
+"""
+
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
